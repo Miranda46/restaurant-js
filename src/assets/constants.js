15542dfd8c8752918c4b5fb67c16db380aaca790
Miranda46 <80054292+Miranda46@users.noreply.github.com>
2024-11-09 14:42:32 -0500

    initial +1

---
+++ src/assets/constants.js
@@ -0,0 +1,13 @@
+export { WELCOME_TEXT, CHEF_TEXT, COUNTRY_TEXT, ENJOY_TEXT, WELCOME_TITLE, CHEF_TITLE, ENJOY_TITLE, COUNTRY_TITLE };
+
+const WELCOME_TITLE = 'Welcome to Le Restaurant de la Chienne!'
+const WELCOME_TEXT = 'Welcome the least important restaurant on earth, where you can get everything you want for all the remaining dollars in your bank account! (We only receive payments in cash).'
+
+const CHEF_TITLE = 'Meet the Chef';
+const CHEF_TEXT = 'My name is Michael Schwarzenegger aus Deutschland. I lived during 55 years in Heidelberg, serving delicatessen to everyone who arrived at the castle. Now I also sell aubergines.'
+
+const COUNTRY_TITLE = 'Neue Horizons'
+const COUNTRY_TEXT = 'Ich wohne nicht mehr in Deutschland, rather, I have expanded my horizons and openned my mind. Therefore I am now living in Lichtenstein.'
+
+const ENJOY_TITLE = 'Most importantly: ENJOY!'
+const ENJOY_TEXT = 'I hope that everything here schmecks. Feel free to try my speciality, like meat in a dirty pan or rosé poultry. Enjoy!'
---
+++ src/script.js
@@ -0,0 +1,49 @@
+import * as constants from './assets/constants';
+
+const loadPage = (() => {
+    const home = function() {
+        let content = document.getElementById('content');
+        content.innerHTML = '';
+        
+        let welcome = document.createElement('div');
+        let welcome_title = document.createElement('h3');
+        welcome_title.innerText = constants.WELCOME_TITLE;
+        let welcome_image = document.createElement('img');
+        welcome_image.src = './assets/welcome.png';
+        let welcome_text = document.createElement('p');
+        welcome_text.innerText = constants.WELCOME_TEXT;
+        welcome.appendChild(welcome_title);
+        welcome.appendChild(welcome_image);
+        welcome.appendChild(welcome_text);
+
+
+
+        
+        let chef = document.createElement('div');
+        compose(chef, constants.CHEF_TITLE, '', constants.CHEF_TEXT, content);
+
+        let country = document.createElement('div');
+        compose(country, constants.COUNTRY_TITLE, '', constants.COUNTRY_TEXT, content);
+
+        let enjoy = document.createElement('div');
+        compose(enjoy, constants.ENJOY_TITLE, '', constants.ENJOY_TEXT, content);
+
+
+        let compose = function(parent, title, imgSrc, text, content){
+            let titleComp = document.createElement('h3');
+            titleComp.innerText = title;
+            let imageComp = document.createElement('img');
+            imageComp.src = imgSrc;
+            let textComp = document.createElement('p');
+            textComp.innerText = text;
+            parent.appendChild(titleComp);
+            parent.appendChild(imageComp);
+            parent.appendChild(textComp);
+            content.appendChild(parent);
+        }
+    }
+
+    const aboutUs = function() { 
+        
+    }
+})();
